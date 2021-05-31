1ee86fc338f9102b6f65b91ef0384940f7ee6f5c
HamzaAhmad9 <>
2021-05-31 08:58:33 +0000

    edited

--- script.js
+++ script.js
@@ -2,16 +2,35 @@
 let userInput = prompt("Hi, please enter the name of your favorite football club and the number of UEFA champions league titles they won separated by a space\nExample: Barca 5");
 
 let usrNumber = document.querySelector("span");
+//let sec = document.querySelector("section");
+//var myImg = document.createElement('img');
+//myImg.src = "https://www.pngarts.com/files/5/UEFA-Champions-League-Trophy-Transparent-Image.png";
 
 userInput = userInput.split(" ");
 //console.log(userInput);
+
+while (!(userInput[0].constructor == String && !isNaN(Number(userInput[1])))) {
+  userInput = prompt("Sorry, please check your entry.");
+}
+
 if (userInput[0].constructor == String && !isNaN(userInput[1]) ) {
-  for (let i = 0; i < userInput[1]; i++){
+
+  //myImg.style.maxWidth = String(90 / Number(userInput[1])) + "%";
+
+  for (let i = 0; i < Number(userInput[1]); i++){
     document.write("<img src='https://www.pngarts.com/files/5/UEFA-Champions-League-Trophy-Transparent-Image.png'>");
+    //sec.appendChild(myImg);
+    //console.log(i);
   }
 
   usrNumber.innerHTML = userInput[1];
+
 }
+/*document.write("<img src='https://www.pngarts.com/files/5/UEFA-Champions-League-Trophy-Transparent-Image.png'>");*/
+
+
+
+
 
 
 //console.log(usrNumber.innerHTML);
